c9b0fe657e3e4f1c3b501dbd5b1b07b67a2ffb1e
Busetdah <apriantowpj@gmail.com>
2025-02-15 19:38:22 +0700

    add for get response value datetime

--- app/Http/Controllers/HistoryTrendController.php
+++ app/Http/Controllers/HistoryTrendController.php
@@ -9,15 +9,56 @@ class HistoryTrendController extends Controller
 {
     public function index(Request $request)
     {
-        $limit = $request->query('limit', 100);
-        $page  = $request->query('page', 1);
+        $limit  = $request->query('limit', 100);
+        $page   = $request->query('page', 1);
         $offset = ($page - 1) * $limit;
 
-        $temphumd = DB::select("SELECT temp, humd, waktu FROM temphumd_sekitar ORDER BY waktu DESC LIMIT ? OFFSET ?", [$limit, $offset]);
-        $weigher  = DB::select("SELECT weigher, waktu FROM data_weigher ORDER BY waktu DESC LIMIT ? OFFSET ?", [$limit, $offset]);
-        $pressure = DB::select("SELECT pressure, timestamp as waktu FROM predicted_data ORDER BY timestamp DESC LIMIT ? OFFSET ?", [$limit, $offset]);
-        $lpvsv1   = DB::select("SELECT lpvweigher1, svweigher1, waktu FROM lpvsvweigher1 ORDER BY waktu DESC LIMIT ? OFFSET ?", [$limit, $offset]);
-        $lpvsv2   = DB::select("SELECT lpvweigher2, svweigher2, waktu FROM lpvsvweigher2 ORDER BY waktu DESC LIMIT ? OFFSET ?", [$limit, $offset]);
+        $start = $request->query('start');
+        $end   = $request->query('end');  
+
+        $dateFilterTemphumd = "";
+        $dateFilterWeigher  = "";
+        $dateFilterPressure = "";
+        $dateFilterLpvsv1   = "";
+        $dateFilterLpvsv2   = "";
+        
+        $bindingsTemphumd = [];
+        $bindingsWeigher  = [];
+        $bindingsPressure = [];
+        $bindingsLpvsv1   = [];
+        $bindingsLpvsv2   = [];
+
+        if ($start && $end) {
+            $dateFilterTemphumd = "WHERE waktu BETWEEN ? AND ?";
+            $dateFilterWeigher  = "WHERE waktu BETWEEN ? AND ?";
+            $dateFilterPressure = "WHERE timestamp BETWEEN ? AND ?";
+            $dateFilterLpvsv1   = "WHERE waktu BETWEEN ? AND ?";
+            $dateFilterLpvsv2   = "WHERE waktu BETWEEN ? AND ?";
+
+            $bindingsTemphumd = [$start, $end];
+            $bindingsWeigher  = [$start, $end];
+            $bindingsPressure = [$start, $end];
+            $bindingsLpvsv1   = [$start, $end];
+            $bindingsLpvsv2   = [$start, $end];
+        }
+
+        $bindingsTemphumd = array_merge($bindingsTemphumd, [$limit, $offset]);
+        $bindingsWeigher  = array_merge($bindingsWeigher, [$limit, $offset]);
+        $bindingsPressure = array_merge($bindingsPressure, [$limit, $offset]);
+        $bindingsLpvsv1   = array_merge($bindingsLpvsv1, [$limit, $offset]);
+        $bindingsLpvsv2   = array_merge($bindingsLpvsv2, [$limit, $offset]);
+
+        $temphumdQuery = "SELECT temp, humd, waktu FROM temphumd_sekitar $dateFilterTemphumd ORDER BY waktu DESC LIMIT ? OFFSET ?";
+        $weigherQuery  = "SELECT weigher, waktu FROM data_weigher $dateFilterWeigher ORDER BY waktu DESC LIMIT ? OFFSET ?";
+        $pressureQuery = "SELECT pressure, timestamp as waktu FROM predicted_data $dateFilterPressure ORDER BY timestamp DESC LIMIT ? OFFSET ?";
+        $lpvsv1Query   = "SELECT lpvweigher1, svweigher1, waktu FROM lpvsvweigher1 $dateFilterLpvsv1 ORDER BY waktu DESC LIMIT ? OFFSET ?";
+        $lpvsv2Query   = "SELECT lpvweigher2, svweigher2, waktu FROM lpvsvweigher2 $dateFilterLpvsv2 ORDER BY waktu DESC LIMIT ? OFFSET ?";
+
+        $temphumd = DB::select($temphumdQuery, $bindingsTemphumd);
+        $weigher  = DB::select($weigherQuery, $bindingsWeigher);
+        $pressure = DB::select($pressureQuery, $bindingsPressure);
+        $lpvsv1   = DB::select($lpvsv1Query, $bindingsLpvsv1);
+        $lpvsv2   = DB::select($lpvsv2Query, $bindingsLpvsv2);
 
         $mergedData = [];
         $count = count($temphumd);
@@ -29,9 +70,9 @@ class HistoryTrendController extends Controller
                 'weigher'   => $weigher[$i]->weigher   ?? null,
                 'pressure'  => $pressure[$i]->pressure ?? null,
                 'bucket1PV' => $lpvsv1[$i]->lpvweigher1 ?? null,
-                'bucket1SV' => $lpvsv1[$i]->svweigher1 ?? null,
+                'bucket1SV' => $lpvsv1[$i]->svweigher1  ?? null,
                 'bucket2PV' => $lpvsv2[$i]->lpvweigher2 ?? null,
-                'bucket2SV' => $lpvsv2[$i]->svweigher2 ?? null,
+                'bucket2SV' => $lpvsv2[$i]->svweigher2  ?? null,
             ];
         }
 
