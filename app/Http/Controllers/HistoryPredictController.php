2a10f6f6591d24010e7a97156368267c4d084f56
Busetdah <apriantowpj@gmail.com>
2025-02-16 21:30:59 +0700

    get all data by pagination

--- app/Http/Controllers/HistoryPredictController.php
+++ app/Http/Controllers/HistoryPredictController.php
@@ -8,16 +8,16 @@ use Illuminate\Support\Facades\DB;
 class HistoryPredictController extends Controller
 {
     public function historyPredict(Request $request)
-{
-    $query = DB::table('data_training');
+    {
+        $query = DB::table('data_training');
 
-    if ($request->has('start_date') && $request->has('end_date')) {
-        $query->whereBetween('waktu', [$request->start_date, $request->end_date]);
+        if ($request->has('start_date') && $request->has('end_date')) {
+            $query->whereBetween('waktu', [$request->start_date, $request->end_date]);
+        }
+
+        $historypredict = $query->paginate(100); 
+
+        return response()->json($historypredict, 200);
     }
-
-    $historypredict = $query->limit(100)->get();
-
-    return response()->json($historypredict, 200);
-}
 
 }
