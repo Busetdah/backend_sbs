f881b66aed2944c1c82a0fc828abdd22f4742c99
Busetdah <apriantowpj@gmail.com>
2025-02-16 21:15:49 +0700

    Fix typo

--- app/Http/Controllers/HistoryPredictController.php
+++ app/Http/Controllers/HistoryPredictController.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class DataTrainingController extends Controller
+class HistoryPredictController extends Controller
 {
     public function historyPredict()
     {
-        $dataTraining = DB::table('data_training')->get();
+        $historypredict = DB::table('data_training')->get();
 
-        if ($dataTraining->isEmpty()) {
+        if ($historypredict->isEmpty()) {
             return response()->json(['message' => 'Tidak ada data dalam history'], 200);
         }
 
-        return response()->json($dataTraining, 200);
+        return response()->json($historypredict, 200);
     }
 }
