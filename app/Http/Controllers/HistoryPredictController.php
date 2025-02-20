4a95cc1d4d09ad609fd52c2f7e75c363c4df411b
Busetdah <apriantowpj@gmail.com>
2025-02-20 22:19:57 +0700

    change controller class

--- app/Http/Controllers/HistoryPredictController.php
+++ app/Http/Controllers/HistoryPredictController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class HistoryTrendController extends Controller
+class HistoryPredictController extends Controller
 {
     public function index(Request $request)
     {
