ba2e0719b05527d51e87f37fd890c4cf043f58ec
Busetdah <apriantowpj@gmail.com>
2025-02-28 12:33:41 +0700

    make sensorData sse

--- app/Http/Controllers/SensorDataController.php
+++ app/Http/Controllers/SensorDataController.php
@@ -5,23 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class SensorDataController extends Controller
 {
-    public function getData(Request $request)
+    public function streamData(Request $request)
     {
-        $query = DB::table('temphumd_produk')
-            ->select('temp', 'humd', 'waktu')
-            ->orderBy('waktu', 'desc');
+        $response = new StreamedResponse(function () use ($request) {
+            while (true) {
+                $query = DB::table('temphumd_produk')
+                    ->select('temp', 'humd', 'waktu')
+                    ->orderBy('waktu', 'desc');
 
-        if ($request->has('start') && $request->has('end')) {
-            $start = Carbon::parse($request->start);
-            $end = Carbon::parse($request->end);
-            $query->whereBetween('waktu', [$start, $end]);
-        } else {
-            $query->limit(100);
-        }
+                if ($request->has('start') && $request->has('end')) {
+                    $start = Carbon::parse($request->start);
+                    $end = Carbon::parse($request->end);
+                    $query->whereBetween('waktu', [$start, $end]);
+                } else {
+                    $query->limit(100);
+                }
 
-        return response()->json($query->get());
+                $data = $query->get();
+
+                echo "data: " . json_encode($data) . "\n\n";
+                ob_flush();
+                flush();
+
+                sleep(1);
+            }
+        });
+
+        $response->headers->set('Content-Type', 'text/event-stream');
+        $response->headers->set('Cache-Control', 'no-cache');
+        $response->headers->set('Connection', 'keep-alive');
+
+        return $response;
     }
 }
